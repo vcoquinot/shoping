aa89bdf907694c2969f038e19d93f99cfcb3f720
vcoquinot <vcoquinot@gmail.com>
2018-02-16 12:07:46 +0100

    text in buttons + onclick

--- Shoping/app/src/main/java/com/example/morga/shoping/categorie_ingredient.java
+++ Shoping/app/src/main/java/com/example/morga/shoping/categorie_ingredient.java
@@ -1,7 +1,12 @@
 package com.example.morga.shoping;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class categorie_ingredient extends AppCompatActivity {
 
@@ -9,5 +14,47 @@ public class categorie_ingredient extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_30categorie_ingredient);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void clickFruit(View fruitImageButton) {
+        Intent intent = new Intent(categorie_ingredient.this, liste_ingredients.class);
+        startActivity(intent);
+    }
+
+    public void clickLegume(View legumeImageButton) {
+        Intent intent = new Intent(categorie_ingredient.this, liste_ingredients.class);
+        startActivity(intent);
+    }
+
+    public void clickBoucherie(View boucherieImageButton) {
+        Intent intent = new Intent(categorie_ingredient.this, liste_ingredients.class);
+        startActivity(intent);
+    }
+
+    public void clickPoissonnerie(View poissonnerieImageButton) {
+        Intent intent = new Intent(categorie_ingredient.this, liste_ingredients.class);
+        startActivity(intent);
+    }
+
+    public void clickFrais(View fraisImageButton) {
+        Intent intent = new Intent(categorie_ingredient.this, liste_ingredients.class);
+        startActivity(intent);
+    }
+
+    public void clickEpicerie(View epicerieImageButton) {
+        Intent intent = new Intent(categorie_ingredient.this, liste_ingredients.class);
+        startActivity(intent);
+    }
+
+    public void clickBoisson(View boissonImageButton) {
+        Intent intent = new Intent(categorie_ingredient.this, liste_ingredients.class);
+        startActivity(intent);
     }
 }
