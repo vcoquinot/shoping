7c71fffae42aed141dca1c444547967da7c8569b
Marc <imac@iMac-de-Marc.local>
2018-02-17 18:12:09 +0100

    écran 51 (remplace le 50)

---
+++ Shoping/app/src/main/java/com/example/morga/shoping/Aide_mentions.java
@@ -0,0 +1,13 @@
+package com.example.morga.shoping;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Aide_mentions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_51aide_mentions);
+    }
+}
