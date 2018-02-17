48244facccfe98848414b6400e3670d089e0935f
Marc <imac@iMac-de-Marc.local>
2018-02-17 12:01:10 +0100

    Marc écran 32 (ex 31 + pop-up)

---
+++ Shoping/app/src/main/java/com/example/morga/shoping/Ingredients.java
@@ -0,0 +1,13 @@
+package com.example.morga.shoping;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ingredients extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_32ingredients);
+    }
+}
