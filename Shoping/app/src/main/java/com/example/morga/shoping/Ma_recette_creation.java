5a62c39a7a7cd81143b5ad7076b45ad7b9cecb94
vcoquinot <vcoquinot@gmail.com>
2018-02-18 21:06:36 +0100

    menu on activity 22

--- Shoping/app/src/main/java/com/example/morga/shoping/Ma_recette_creation.java
+++ Shoping/app/src/main/java/com/example/morga/shoping/Ma_recette_creation.java
@@ -1,9 +1,11 @@
 package com.example.morga.shoping;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 /**
  * Created by valerieoquinot on 17/02/2018.
@@ -16,6 +18,33 @@ public class Ma_recette_creation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_23ma_recette_creation);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()== R.id.ma_selection_menu){
+            Intent selection=new Intent(this,selection_recette.class);
+            startActivity(selection);
+
+        }
+        else if (item.getItemId()==R.id.courses_menu){
+            Intent courses=new Intent(this,mes_courses.class);
+            startActivity(courses);
+        }
+        else if (item.getItemId()==R.id.newIngredient_menu){
+            Intent nvingredient=new Intent(this,liste_ingredients.class);
+            startActivity(nvingredient);
+        }
+        else if (item.getItemId()==R.id.aide_menu){
+            Intent aide=new Intent(this,Aide_mentions.class);
+            startActivity(aide);
+        }
+        else if (item.getItemId()==R.id.recettes_menu){
+            Intent recettes=new Intent(this,categorie_recette.class);
+            startActivity(recettes);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
