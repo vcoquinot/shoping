124e3f7b5d8cd0d15af38b9f300e8fec88b5f2d7
MY-COMPUTER\morga <morgan.fraisse@hotmail.fr>
2018-02-21 09:01:48 +0100

    modif des includes menu

--- Shoping/app/src/main/java/com/example/morga/shoping/mes_courses.java
+++ Shoping/app/src/main/java/com/example/morga/shoping/mes_courses.java
@@ -72,37 +72,7 @@ public class mes_courses extends AbstractActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu,menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId()== R.id.ma_selection_menu){
-            Intent selection=new Intent(this,selection_recette.class);
-            startActivity(selection);
 
-        }
-        else if (item.getItemId()==R.id.courses_menu){
-            Intent courses=new Intent(this,mes_courses.class);
-            startActivity(courses);
-        }
-        else if (item.getItemId()==R.id.newIngredient_menu){
-            Intent nvingredient=new Intent(this,Ingredients.class);
-            startActivity(nvingredient);
-        }
-        else if (item.getItemId()==R.id.aide_menu){
-            Intent aide=new Intent(this,Aide_mentions.class);
-            startActivity(aide);
-        }
-        else if (item.getItemId()==R.id.recettes_menu){
-            Intent recettes=new Intent(this,categorie_recette.class);
-            startActivity(recettes);
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
 
 }
