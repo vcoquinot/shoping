78fe72183162c03a68736f772fb2fc5398971894
vcoquinot <vcoquinot@gmail.com>
2018-02-26 10:04:28 +0100

    click categories recettes + intent activity

--- Shoping/app/src/main/java/com/example/morga/shoping/categorie_recette20.java
+++ Shoping/app/src/main/java/com/example/morga/shoping/categorie_recette20.java
@@ -18,26 +18,30 @@ public class categorie_recette20 extends AbstractActivity {
 
 
     public void clickEntree(View entreeImageButton) {
-        Intent activity_entree = new Intent(this, Liste_recettes21.class);
-        activity_entree.putExtra("cat_recettes", "ENTRÉE");
+        Intent activity_entree = new Intent(categorie_recette20.this, Liste_recettes21.class);
+        activity_entree.putExtra("cat_recettes", "entrée");
         activity_entree.putExtra(Intent.EXTRA_TEXT, ContentText);
-
         startActivity(activity_entree);
-
     }
 
     public void clickPlat(View platImageButton) {
-//        Intent intent = new Intent(categorie_recette20.this, Liste_recettes21.class);
-//        startActivity(intent);
+        Intent activity_plat = new Intent(categorie_recette20.this, Liste_recettes21.class);
+        activity_plat.putExtra("cat_recettes", "plat");
+        activity_plat.putExtra(Intent.EXTRA_TEXT, ContentText);
+        startActivity(activity_plat);
     }
 
     public void clickDessert(View dessertImageButton) {
-//        Intent intent = new Intent(categorie_recette20.this, Liste_recettes21.class);
-//        startActivity(intent);
+        Intent activity_dessert = new Intent(categorie_recette20.this, Liste_recettes21.class);
+        activity_dessert.putExtra("cat_recettes", "dessert");
+        activity_dessert.putExtra(Intent.EXTRA_TEXT, ContentText);
+        startActivity(activity_dessert);
     }
 
     public void clickAutre(View autreImageButton) {
-//        Intent intent = new Intent(categorie_recette20.this, Liste_recettes21.class);
-//        startActivity(intent);
+        Intent activity_autre = new Intent(categorie_recette20.this, Liste_recettes21.class);
+        activity_autre.putExtra("cat_recettes", "autre");
+        activity_autre.putExtra(Intent.EXTRA_TEXT, ContentText);
+        startActivity(activity_autre);
     }
 }
