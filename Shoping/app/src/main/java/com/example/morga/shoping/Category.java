06288237017875c3f073cb6d0e89eaafe822895d
MY-COMPUTER\morga <morgan.fraisse@hotmail.fr>
2018-02-27 16:43:31 +0100

    Constantes terminées

--- Shoping/app/src/main/java/com/example/morga/shoping/Category.java
+++ Shoping/app/src/main/java/com/example/morga/shoping/Category.java
@@ -9,4 +9,12 @@ public class Category {
     public static final String PLAT= "Plat";
     public static final String DESSERT= "Dessert";
     public static final String AUTRES= "Autres";
+    public static final String FRUITS= "FRUITS";
+    public static final String LEGUMES= "LÉGUMES";
+    public static final String BOUCHERIE= "BOUCHERIE";
+    public static final String POISSONNERIE= "POISSONNERIE";
+    public static final String EPICERIE= "ÉPICERIE";
+    public static final String FRAIS= "FRAIS";
+    public static final String BOISSONS= "BOISSONS";
 }
+
